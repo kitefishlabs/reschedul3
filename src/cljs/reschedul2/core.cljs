001c0b61102b51f527eccef4ba9e79a45d043d88
Tom Stoll <tms@kitefishlabs.com>
2017-01-07 02:06:42 -0500

    Start adding functionality to main page elements.

--- src/cljs/reschedul2/core.cljs
+++ src/cljs/reschedul2/core.cljs
@@ -8,7 +8,8 @@
             [ajax.core :refer [GET POST]]
             [reschedul2.ajax :refer [load-interceptors!]]
             [reschedul2.handlers]
-            [reschedul2.subscriptions])
+            [reschedul2.subscriptions]
+            [reschedul2.views.menusnavs :as navs])
   (:import goog.History))
 
 ; (defn nav-link [uri title page collapsed?]
@@ -18,220 +19,6 @@
 ;      [:a.nav-link
 ;       {:href uri
 ;        :on-click #(reset! collapsed? true)} title]]))
-
-(defn user-image
-  [img-url width-css]
-  [:img.img-circle
-    { :src img-url
-      :alt "User Image"
-      :width width-css}])
-
-(defn treeview-menu-item
-  [href value]
-  [:li
-    [:a
-      {:href href}
-      [:i.fa.fa-circle-o]
-      value]])
-
-(defn main-navbar []
-  [:nav.navbar.navbar-static-top
-    {:role "navigation"}
-
-    [:a.sidebar-toggle
-      {:href "#"
-       :data-toggle "offcanvas"
-       :role "button"}
-      [:span.sr-only "Toggle navigation"]]
-
-    ; <!-- Navbar Right Menu -->
-    [:div.navbar-custom-menu
-      [:ul.nav.navbar-nav
-        [:li.dropdown.messages-menu
-          [:a.dropdown-toggle
-            {:href "#"
-             :data-toggle "dropdown"}
-            [:i.fa.fa-envelope-o]
-            [:span.label.label-success "4"]]
-
-          [:ul.dropdown-menu
-            [:li.header "You have 4 messages"]
-            ; <!-- inner menu: contains the messages -->
-            [:li
-              [:ul.menu
-                [:li
-                  [:a {:href "#"}
-                    [:div.pull-left
-                      [user-image "img/user2-160x160.jpg" "inherit"]]
-                    [:h4;
-                      "Support Team"
-                      [:small
-                        [:i.fa.fa-clock-o]
-                        " 5 mins"]]
-                    [:p "THE MESSAGE."]]]
-                [:li.footer
-                  [:a
-                    {:href "#"}
-                    "See All Messages"]]]]]]
-        [:li.dropdown.notifications-menu
-          ; <!-- Menu toggle button -->
-          [:a.dropdown-toggle
-            { :href "#"
-              :data-toggle "dropdown"}
-            [:i.fa.fa-bell-o]
-            [:span.label.label-warning "10"]]
-          ; <!-- Dropdown Menu -->
-          [:ul.dropdown-menu
-            [:li.header
-              "You have 10 notifications"]
-            [:li
-              ; <!-- Inner Menu: contains the notifications -->
-              [:ul.menu
-                 [:li ; <!-- start notification -->
-                  [:a { :href "#"}
-                    [:i.fa.fa-users.text-aqua]
-                    "5 new members joined today"]]]]
-            ; <!-- end notification -->]
-            [:li.footer
-              [:a { :href "#" } "View all"]]]]
-
-          ; Skip Tasks Menu, other menus, add as needed
-
-          ; <!-- User Account Menu -->
-        [:li.dropdown.user.user-menu
-          ; <!-- Menu Toggle Button -->
-          [:a.dropdown-toggle
-            { :href "#"
-              :data-toggle "dropdown"}
-              ; <!-- The user image in the navbar-->
-            [user-image "img/user2-160x160.jpg" "64px"]
-            ; <!-- hidden-xs hides the username on small devices so only the image appears. -->
-            [:span.hidden-xs "I. N. Fring-Freeley"]]
-
-
-          [:ul.dropdown-menu
-            ; <!-- The user image in the menu -->
-            [:li.user-header
-              [user-image "img/user2-160x160.jpg" "64px"]
-              [:p "I. N. Fring-Freeley - Barriere"
-                [:small "Meta-organizer since Nov. 2005(ish)"]]]
-
-            ;<!-- Menu Body -->
-            [:li.user-body
-              [:div.row
-                [:div.col-xs-4.text-center
-                  [:a { :href "#"}
-                    "Info"]]
-                [:div.col-xs-4.text-center
-                  [:a { :href "#"}
-                    "Proposals"]]
-                [:div.col-xs-4.text-center
-                  [:a { :href "#"}
-                    "Contact"]]]]
-
-            ;<!-- Menu Footer -->
-            [:li.user-footer
-
-              [:div.pull-left
-                [:a.btn.btn-xs.btn-default.btn-flat
-                  { :href "#"}
-                  "Profile"]]
-              [:div.pull-right
-                [:a.btn.btn-xs.btn-default.btn-flat
-                  { :href "#"}
-                  "Sign Out"]]]]]]]])
-
-
-
-(defn main-header []
-  [:header.main-header
-    [:a.logo
-      { :href "#"}
-      [:span.logo-mini [:b "A"] "LT"]         ; mini logo for sidebar mini 50x50 pixels
-      [:span.logo-lg [:b "Admin"] "LTE"]]   ; logo for regular state and mobile devices
-    [main-navbar]])
-
-(defn main-sidebar []
-  [:aside.main-sidebar
-
-    [:section.sidebar
-
-      [:div.user-panel
-        [:div.pull-left.image
-          [user-image "img/user2-160x160.jpg" "inherit"]]
-        [:div.pull-left.info
-          [:p "I.N.Fring-Freeley"]
-          [:a { :href "#"}
-            [:i.fa.fa-circle.text-success]
-            "Online"]]]
-
-      [:form.sidebar-form
-        { :action "#"
-          :method "get"}
-        [:div.input-group
-          [:input
-            { :type "text"
-              :name "q"
-              :placeholder "Search..."}]
-          [:span.input-group-btn
-            [:button.btn.btn-flat
-              { :type "submit"
-                :name "search"
-                :id "search-btn"}
-              [:i.fa.fa-search]]]]]
-
-      ; <!-- Sidebar Menu -->
-      [:ul.sidebar-menu
-
-        [:li.header "MAIN NAVIGATION"]
-
-        ; <!-- Optionally, you can add icons to the links -->
-        [:li.active.treeview
-
-          [:a
-            { :href "#"}
-            [:i.fa.fa-dashboard]
-            [:span "Scheduling Dashboard"]
-            [:span.pull-right-container
-              [:i.fa.fa-angle-left.pull-right]]]
-
-          [:ul.treeview-menu
-            [treeview-menu-item "users" "Users"]
-            [treeview-menu-item "venues" "Venues"]
-            [treeview-menu-item "proposals" "Proposals"]
-            [treeview-menu-item "shows" "Shows"]]]
-
-        [:li.treeview
-          [:a { :href "#"}
-            [:i.fa.fa-files-o]
-            [:span "Communicate"]
-            [:span.pull-right-container
-              [:span.label.label-primary.pull-right "4"]]]
-
-          [:ul.treeview-menu
-            [treeview-menu-item
-              "#/email-all-orgs"
-              "Email all organizers."]
-            [treeview-menu-item
-              "#/email-devs"
-              "Email developers."]
-            [treeview-menu-item
-              "#/suggest-a-slack"
-              "Suggest a Slack channel."]
-            [treeview-menu-item
-              "#/email-anonymously"
-              "Share some intel."]]]]]])
-
-
-(defn footer []
-  [:footer.main-footer
-    [:div.pull-right.hidden-xs
-      "BiF 2017"]
-    [:strong
-      "Copyright 2016 "
-      [:a
-        {:href "about#contributors"}
-        "by the authors."]]])
 
 (defn about-page []
   [:div.content-wrapper
@@ -247,28 +34,29 @@
 
 (defn home-page []
   [:div.content-wrapper
+
     [:section.content-header
       [:ol.breadcrumb
         [:li
           [:a {:href "#"}
             [:i.fa.fa-dashboard]
-            "Level"]]
-        [:li.active "Here"]]
-      [:h1 "Home"
-        [:small "Optional description"]]]
+            "reschedul2"]]
+        [:li.active "Home"]]
+      [:h1 "Home"]]
+
     [:section.content
-      [:p "CONTENT"]]])
+      [:p "Welcome to Infringement"]]])
 
 (def pages
   {:home #'home-page
    :about #'about-page})
 
 (defn page []
-  [:div
-   [main-header]
-   [main-sidebar]
+  [:div.wrapper
+   [navs/main-header]
+   [navs/main-sidebar]
    [(pages @(rf/subscribe [:page]))]
-   [footer]])
+   [navs/main-footer]])
 
 ;; -------------------------
 ;; Routes
