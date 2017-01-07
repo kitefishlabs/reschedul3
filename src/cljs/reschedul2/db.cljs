f02aab45ac3997f665555ea6a7058fa86c851590
Tom Stoll <tms@kitefishlabs.com>
2017-01-07 02:11:08 -0500

    remove db fake record from front end

--- src/cljs/reschedul2/db.cljs
+++ src/cljs/reschedul2/db.cljs
@@ -1,18 +1,4 @@
 (ns reschedul2.db)
 
 (def default-db
-  { :page :home
-    :default-user
-      { :_id "5861674414c82274d94c7fd5"
-        :username "tms"
-        :pass "xxx"
-        :first_name "Tom"
-        :last_name "Stoll"
-        :admin true
-        :role "admin"
-        :contact-info
-          { :cell-phone "999-9999"
-            :second-phone "888-7777"
-            :email "tms@kitefishlabs.com"
-            :address ""
-            :preferred_contact_method "email"}}})
+  { :page :home})
